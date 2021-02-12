2f396e76d7d1b4547bdada4b9810ab6ee18c2f07
riricado <78606227+riricado@users.noreply.github.com>
2021-02-12 12:21:41 +0100

    changement de tours
    changement de tours + choix aléatoire pour l'IA

--- IA.py
+++ IA.py
@@ -1,12 +1,17 @@
+
 from Player import *
+from Chessgame import *
+
 
 class IA(Player):
 
     def __init__(self, name: str):
         super().__init__(name)
         
-    def pickMove(self):
-        pass
+    def pickMove(self, board):
+        legal_moves = list(board.legal_moves)
+        return legal_moves[0]
+        
 
     def evaluate(self, board):
         pass
